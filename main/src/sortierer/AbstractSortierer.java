bf91dbd9506203bb883717a54944489c4b54eed5
christ_sve <chrsve@gmx.de>
2022-01-27 08:55:30 +0100

    fixed AbstractSortierer

--- main/src/sortierer/AbstractSortierer.java
+++ main/src/sortierer/AbstractSortierer.java
@@ -2,13 +2,9 @@ package sortierer;
 
 public abstract class AbstractSortierer {
 
-    static int[] sort(int[] input) {
-        return new int[0];
-    }
-
     static boolean isValid(int[] input) {
         if (input != null) {
-            if(input.length > 1) {
+            if (input.length > 1) {
                 return true;
             }
         }
