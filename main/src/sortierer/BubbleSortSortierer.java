3d0f42245025df87b33dcd0d35471c057549683a
christ_sve <chrsve@gmx.de>
2022-02-03 10:19:38 +0100

    Insertionsort erklärung, dokumentation, verbesserte testansicht

--- main/src/sortierer/BubbleSortSortierer.java
+++ main/src/sortierer/BubbleSortSortierer.java
@@ -2,6 +2,18 @@ package sortierer;
 
 public class BubbleSortSortierer extends AbstractSortierer {
 
+    /**
+     * Der simpelste Sortieralgorithmus, der Sortieralgorithmus
+     * vergleicht jeweils zwei nebeneinanderliegende Werte und
+     * tauscht entsprechend der Größe. Dadurch, dass theoretisch
+     * für n Werte n Durchläufe mit jeweils n Vergleichen benötigt
+     * werden, entspricht die Laufzeit O=(n^2) und ist die schlechteste
+     * aller Sortieralgorithmen.
+     *
+     *
+     * @param input
+     * @return
+     */
     public static int[] sort(int[] input) {
         if (!isValid(input)) {
             return new int[]{};
