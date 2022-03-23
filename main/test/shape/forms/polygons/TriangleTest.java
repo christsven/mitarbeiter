36438cc678263931b9107f753a698b4605096dc1
christ_sve <chrsve@gmx.de>
2022-03-23 09:23:11 +0100

    test

--- main/test/shape/forms/polygons/TriangleTest.java
+++ main/test/shape/forms/polygons/TriangleTest.java
@@ -1,12 +1,51 @@
 package shape.forms.polygons;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class TriangleTest {
 
-    //TODO create triangle with correct values
-    //TODO change correct value to another correct value
-    //TODO create triangle with false values throws exception
-    //TODO change correct value to false value throws exception
+    @Test
+    public void triangle_correct_values() {
+        Assertions.assertDoesNotThrow(() -> {
+            Triangle sut = new Triangle(1, 2, 2);
+        });
+    }
+
+    @Test
+    public void triangle_parameter_changed() {
+        //given
+        Triangle sut = new Triangle(1, 2, 2);
+        double before = sut.getCircumference();
+    }
+
+    @Test
+    public void triangle_change_correct_values() {
+        //given
+        Triangle sut = new Triangle(5, 5, 5);
+
+        //when
+        Assertions.assertDoesNotThrow(() -> {
+            sut.setB(7);
+        });
+    }
+
+    @Test
+    public void triangle_invalid_values() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Triangle sut = new Triangle(1, 100, 3);
+        });
+    }
+
+    @Test
+    public void triangle_change_correct_values_to_invalid_values() {
+        //given
+        Triangle sut = new Triangle(5, 5, 5);
+
+        //when
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            sut.setA(190);
+        });
+    }
 
 }
