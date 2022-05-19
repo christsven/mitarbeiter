98f77297b1daa4172912412989f782e0a213bc12
christ_sve <chrsve@gmx.de>
2022-05-19 12:52:59 +0200

    Iot

--- main/src/main/MainClass.java
+++ main/src/main/MainClass.java
@@ -1,9 +1,30 @@
 package main;
 
+import person.mitarbeiter.BueroArbeiter;
+import person.mitarbeiter.Manager;
+import person.mitarbeiter.Mitarbeiter;
+import person.mitarbeiter.SchichtArbeiter;
+
+import java.util.*;
+
 class MainClass {
 
     public static void main(String[] args){
 
+        Set<Mitarbeiter> set = new TreeSet<Mitarbeiter>();
+        ArrayList<Mitarbeiter> set1 = new ArrayList<SchichtArbeiter>();
+        List<Mitarbeiter> set2 = new ArrayList<Mitarbeiter>();
+        LinkedList<BueroArbeiter> set3 = new LinkedList<Manager>();
+        List<BueroArbeiter> set4 = new LinkedList<BueroArbeiter>();
 
     }
 }
+
+
+
+
+
+
+
+
+
