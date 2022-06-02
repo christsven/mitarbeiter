081dc811d9e8bae252a067a970c505c430c4f2fa
christ_sve <chrsve@gmx.de>
2022-06-02 11:59:50 +0200

    remove unneccesary code

--- main/src/main/MainClass.java
+++ main/src/main/MainClass.java
@@ -1,14 +1,9 @@
 package main;
 
-import object.TrianglePrism;
-import shape.Triangle;
-
 class MainClass {
 
     public static void main(String[] args) {
 
-        TrianglePrism prism = new TrianglePrism(10, new Triangle(10, 10, 10), 10);
-        System.out.println(prism.getSurface());
     }
 }
 
